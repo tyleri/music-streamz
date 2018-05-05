07d171447f6db80bfce38dfa3cdf0812c1e9b1e3
Tyler Ishikawa <tyi3@cornell.edu>
2018-05-05 17:49:29 -0400

    add http error message to console log

--- backend/music_streamz/helpers.py
+++ backend/music_streamz/helpers.py
@@ -244,7 +244,7 @@ def get_recommendations(list_picked_songs, limit=4):
 
   r = requests.get(url, params=params, headers=headers)
   if r.status_code != 200:
-    raise ValueError('Error querying Spotify')
+    raise ValueError('Error querying Spotify\n' + r.text)
 
   results = r.json()['tracks']
 
